b9e6e78b8dcefecdcd1255f26d929113b0cbd05f
NeonPokharkar <neonpokharkar@gmail.com>
2025-06-02 17:13:06 +0530

    Changing return type to json 2

---
+++ src/main/java/org/launch/edge/models/ResponseModel.java
@@ -0,0 +1,14 @@
+package org.launch.edge.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseModel {
+    String message;
+}
